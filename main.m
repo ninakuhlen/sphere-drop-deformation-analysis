30c3a6644bd46eb21e50b817d67e823e4676a591
Nina Kuhlen <ninakn78@gmail.com>
2024-12-12 18:55:24 +0100

    feature: add main.m to run main code

--- main.m
+++ main.m
@@ -1,13 +1,24 @@
 % Hauptskript
 
+% Konfiguration importieren
+load('configs/configs.mat'); % Lädt die Variablen aus der configs.mat-Datei
+
 % 1. Video laden und Frames extrahieren
-videoProcessor = VideoProcessor('path_to_video.avi');
+videoProcessor = VideoProcessor(videoPath);
+fprintf('Create VideoProcessor object...\n');
 videoProcessor = videoProcessor.extractFrames();
 
-% 2. Frames summieren
-frameSummation = FrameSummation(videoProcessor.frames);
-summedMatrix = frameSummation.computeSum();
+% Frames aus Video inspizieren
+% FrameInspector.displayFrameResolution(videoProcessor.extractedFrames);
+% FrameInspector.displayFrame(videoProcessor.extractedFrames, 90);
+% FrameInspector.displayPixelValues(videoProcessor.extractedFrames, 100);
+%FrameInspector.displayFrameRegion(videoProcessor.extractedFrames, 90, 300:1000, 350:1650);
 
-% 3. Visualisierung
-Visualization.displaySummedMatrix(summedMatrix); % Summierte Matrix anzeigen
-Visualization.displayFrame(videoProcessor.frames(:, :, 1), 1); % Ersten Frame anzeigen
+
+% % 2. Frames summieren
+% frameSummation = FrameSummation(videoProcessor.extractedFrames);
+% summedMatrix = frameSummation.computeSum();
+% 
+% % 3. Visualisierung
+% Visualization.displaySummedMatrix(summedMatrix); % Summierte Matrix anzeigen
+% Visualization.displayFrame(videoProcessor.extractedFrames(:, :, 30), 30); % 30. Frame anzeigen
