377e92dc939af285fb7ae3b55a3cfbcd5f3aa03c
Nina Kuhlen <ninakn78@gmail.com>
2024-12-15 22:55:26 +0100

    feat: sum brightness values of binarized frames and show in figure

--- FrameConverter.m
+++ FrameConverter.m
@@ -4,28 +4,34 @@ classdef FrameConverter
     end
 
     methods
+        % Constructor
         function obj = FrameConverter(brightnessThreshold)
             obj.brightnessThreshold = brightnessThreshold;
         end
 
-        function binarizedFrame = convert(obj, frame)
+        % Kapselung der Konvertierungsmethoden
+        function binarizedFrame = convert(obj, frameIndex, frame)
             grayFrame = obj.convertToGrayFrame(frame);
-            binarizedFrame = obj.convertToBinaryFrame(grayFrame);
+            binarizedFrame = obj.convertToBinaryFrame(frameIndex, grayFrame);
         end
 
-        function grayFrame = convertToGrayFrame(obj, rgbFrame)
-            if size(rgbFrame, 3) == 3
-                grayFrame = rgb2gray(rgbFrame);
+        % Konvertierungs-Methoden
+        function grayFrame = convertToGrayFrame(~, frame)
+            if size(frame, 3) == 1
+                grayFrame = frame;
+                return;
+            end
+            if size(frame, 3) == 3
+                grayFrame = rgb2gray(frame);
                 return;
             end
 
-            % Alle anderen Kanäle ignorieren
-            grayFrame = rgbFrame;
+            error('invalid amount of bands');
         end
 
-        function binarizedFrame = convertToBinaryFrame(obj, grayFrame)
-            pixelValueMatrix = imbinarize(grayFrame, obj.brightnessThreshold / 255);
-            binarizedFrame = BinarizedFrame(grayFrame, pixelValueMatrix);
+        function binarizedFrameDto = convertToBinaryFrame(obj, frameIndex, grayFrame)
+            binaryFrame = imbinarize(grayFrame, obj.brightnessThreshold / 255); % normalisierter Threshold
+            binarizedFrameDto = BinarizedFrameDto(frameIndex, grayFrame, binaryFrame);
         end
     end
 end
